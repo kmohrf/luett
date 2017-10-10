f70821f4f944bb3986f70c874ab49408af20c301
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-10-10 05:01:11 +0200

    switch from chai to must

--- src/component.test.js
+++ src/component.test.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
-/* global expect:false */
 
 import bel from 'bel'
+import expect from 'must'
 import { component } from './component'
 
 function simpleElement () {
@@ -23,50 +23,49 @@ describe('component', () => {
   const components = component('foo', simpleComponent, simpleElement())
 
   it('accepts a name, looks for [data-component="$name"] elements and returns them initialized', () => {
-    expect(components)
-      .to.be.an('array')
-      .to.have.lengthOf(3)
+    expect(components).to.be.an.array()
+    expect(components).to.have.length(3)
 
-    expect(components[0]).to.have.deep.property('factory', simpleComponent)
+    expect(components[0].factory).to.equal(simpleComponent)
   })
 
   it('passes an element and options object to the factory', () => {
     let callArgs
     component('foo', (...args) => { callArgs = args }, simpleElement())
-    expect(callArgs).to.have.lengthOf(2)
+    expect(callArgs).to.have.length(2)
     expect(callArgs[0]).to.be.an.instanceof(window.HTMLElement)
-    expect(callArgs[1]).to.be.an('object')
+    expect(callArgs[1]).to.be.an.object()
   })
 
   it('adds the component name to the componentReady dataset of the element', () => {
-    expect(components[0]).to.have.deep.property('el.dataset.componentReady').that.contains('foo')
+    expect(components[0].el.dataset.componentReady).to.contain('foo')
   })
 
   it('stores the index of the processed element inside the options', () => {
-    expect(components[0]).to.have.deep.property('opts.index', 0)
-    expect(components[1]).to.have.deep.property('opts.index', 1)
-    expect(components[2]).to.have.deep.property('opts.index', 2)
+    expect(components[0].opts.index).to.be(0)
+    expect(components[1].opts.index).to.be(1)
+    expect(components[2].opts.index).to.be(2)
   })
 
   describe('the passed options.conf object', () => {
     it('merges the factory defaults', () => {
-      expect(components[0]).to.have.deep.property('opts.conf.lang', simpleComponent.DEFAULTS.lang)
+      expect(components[0].opts.conf.lang).to.be(simpleComponent.DEFAULTS.lang)
     })
 
     it('merges options provided on component call', () => {
       const components = component('foo', simpleComponent, { root: simpleElement(), conf: { lang: 'fr' } })
-      expect(components[0]).to.have.deep.property('opts.conf.lang', 'fr')
-      expect(components[1]).to.have.deep.property('opts.conf.lang', 'de')
+      expect(components[0].opts.conf.lang).to.be('fr')
+      expect(components[1].opts.conf.lang).to.be('de')
     })
 
     it('merges options provided via data-component-* attributes', () => {
-      expect(components[1]).to.have.deep.property('opts.conf.lang', 'de')
-      expect(components[1]).to.have.deep.property('opts.conf.langFallback', 'en')
+      expect(components[1].opts.conf.lang).to.be('de')
+      expect(components[1].opts.conf.langFallback).to.be('en')
     })
 
     it('merges options provided via data-$name-* attributes', () => {
-      expect(components[2]).to.have.deep.property('opts.conf.lang', 'en')
-      expect(components[2]).to.have.deep.property('opts.conf.langFallback', 'es')
+      expect(components[2].opts.conf.lang).to.be('en')
+      expect(components[2].opts.conf.langFallback).to.be('es')
     })
   })
 
@@ -76,15 +75,13 @@ describe('component', () => {
     }
 
     it('uses the NAME attribute from the init function', () => {
-      expect(create(simpleComponent))
-        .to.have.deep.property('[0].el.dataset.componentReady')
-        .that.contains(simpleComponent.NAME)
+      expect(create(simpleComponent)[0].el.dataset.componentReady)
+        .to.contain(simpleComponent.NAME)
     })
 
     it('or the name attribute from the provided options', () => {
-      expect(create(el => ({ el }), { name: 'herold' }))
-        .to.have.deep.property('[0].el.dataset.componentReady')
-        .that.contains('herold')
+      expect(create(el => ({ el }), { name: 'herold' })[0].el.dataset.componentReady)
+        .to.contain('herold')
     })
 
     it('throws an exception if neither is provided', () => {
